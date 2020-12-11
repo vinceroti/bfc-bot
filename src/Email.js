13d2d5fde0e2e42df6c6f43df6b314973d5b5154
Vince Roti <vince.roti@gmail.com>
2020-12-11 12:16:13 -0800

    email setup, improved error messaging

---
+++ src/Email.js
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+import chalk from "chalk";
+import "dotenv/config";
+
+const username = process.env["EMAIL_USERNAME"];
+const password = process.env["EMAIL_PASSWORD"];
+const emails = process.env["EMAILS"].split(",");
+
+const transportOptions = {};
+
+if (username && password) {
+  transportOptions.auth = {};
+  transportOptions.auth.user = username;
+  transportOptions.auth.pass = password;
+}
+
+transportOptions.service = "gmail";
+
+export const transporter = nodemailer.createTransport({
+  ...transportOptions,
+});
+
+export function sendEmail(url) {
+  emails.forEach((e) => {
+    if (username && password) {
+      console.log(chalk.yellow("↗ sending email"));
+
+      const mailOptions = {
+        from: username,
+        subject: "Ikon Bot - Your Day is AVAILABLE!",
+        text: `${process.env["IKON_DATE"]} has an opening, go snag it. Also tell Vince he is awesome. - ${url}`,
+        to: e,
+      };
+
+      transporter.sendMail(mailOptions, (error) => {
+        if (error) {
+          console.log(chalk.red("✖ couldn't send email ::", error));
+        } else {
+          console.log(chalk.green("✔ email sent"));
+        }
+      });
+    }
+  });
+}
--- src/IkonBot.js
+++ src/IkonBot.js
@@ -2,6 +2,8 @@ import puppeteer from "puppeteer";
 import fs from "fs/promises";
 import chalk from "chalk";
 import notifier from "node-notifier";
+import date from "validate-date";
+import { sendEmail } from "./Email";
 
 class IkonBot {
   constructor() {
@@ -10,21 +12,25 @@ class IkonBot {
     this.client = "";
     this.baseUrl = "https://account.ikonpass.com";
     this.date = process.env["IKON_DATE"];
+    this.loginUrl = `${this.baseUrl}/en/login`;
     this.userName = process.env["IKON_USERNAME"];
     this.pass = process.env["IKON_PASS"];
+    this.success = false;
+    this.successTries = 0;
     this.init();
   }
   async init() {
     this.browser = await puppeteer.launch();
     this.page = await this.browser.newPage();
     this.client = await this.page.target().createCDPSession();
+    this.validateDate();
     await this.setCookies();
     await this.login();
     await this.getDays();
   }
   async login() {
     try {
-      await this.page.goto(`${this.baseUrl}/en/login`, {
+      await this.page.goto(this.loginUrl, {
         waitUntil: "networkidle0",
       });
       if (this.page.url().includes("myaccount")) {
@@ -40,7 +46,12 @@ class IkonBot {
       console.log(chalk.green("Session Created :: Logged In"));
       await this.saveCookies();
     } catch (error) {
-      console.error(error);
+      console.error(
+        chalk.red(
+          `✖ Failed to login :: Check .env file to make sure you have the correct email and pass. Otherwise, SEEK DEV HELP 😨`
+        )
+      );
+      process.exit();
     }
   }
   async saveCookies() {
@@ -54,7 +65,7 @@ class IkonBot {
       const cookies = JSON.parse(cookiesString);
       await this.page.setCookie(...cookies);
     } catch (error) {
-      console.error(error);
+      console.error(chalk.yellow(`✖ No Cookies found or failed to load`));
     }
   }
   async getDays() {
@@ -71,9 +82,9 @@ class IkonBot {
       const timestamp = new Date().toLocaleTimeString();
       const dates = innerText.data[0].unavailable_dates;
 
-      dates.includes(this.date)
-        ? this.failureNotify(timestamp)
-        : this.successfulNotify(timestamp);
+      !dates.includes(this.date)
+        ? this.failureMsg(timestamp)
+        : this.successNotify(timestamp);
 
       setTimeout(() => {
         this.getDays();
@@ -82,19 +93,40 @@ class IkonBot {
       console.error(error);
     }
   }
-  failureNotify(timestamp) {
-    console.log(chalk.red(`✖ ${timestamp} :: DAY IS UNAVAILABLE`));
+  failureMsg(msg) {
+    console.log(chalk.red(`✖ ${msg} :: DAY IS UNAVAILABLE`));
+    this.resetSuccess();
   }
-  successfulNotify(timestamp) {
+  resetSuccess() {
+    this.success = false;
+    this.successTries = 0;
+  }
+  successMsg(msg) {
+    console.log(chalk.bgGreen.white.bold(msg));
+  }
+  successNotify(timestamp) {
     const message = `🚀🚨 ✔ ${timestamp} :: DAY IS AVAILABLE - SNAG IT UP 🚨🚀`;
-    notifier.notify({
-      message: message,
-      sound: true,
-    });
-    console.log(chalk.bgGreen.white.bold(message));
-    this.sendText();
+    this.successMsg(message);
+    if (this.successTries >= 3) this.resetSuccess();
+
+    if (!this.success) {
+      notifier.notify({
+        message,
+        sound: true,
+      });
+      sendEmail(this.loginUrl);
+      this.success = true;
+    }
+    this.successTries++;
   }
-  sendText() {}
+  validateDate() {
+    if (!date(this.date, "boolean")) {
+      console.error(
+        chalk.red(`✖ INVALID DATE :: Check .env for correct format`)
+      );
+      process.exit();
+    }
+  }
 }
 
 export default IkonBot;
